7618db5a93a66387be14039f12b2ba0ee6c72690
Liam <11602015+Shyadow@users.noreply.github.com>
2019-11-07 17:21:54 +0100

    Move flushing remaining headers when exiting context to close() method

--- cqc/pythonLib.py
+++ cqc/pythonLib.py
@@ -343,10 +343,6 @@ class CQCConnection:
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        # Flush all remaining commands
-        if self._pending_headers:
-            self.flush()
-        
         # All qubits should now be released
         self.close(release_qubits=True)
 
@@ -373,9 +369,16 @@ class CQCConnection:
         return self._appID
 
     def close(self, release_qubits=True):
+        """Handle closing actions.
+
+        Flushes remaining headers, releases all qubits, closes the 
+        connections, and removes the app ID from the used app IDs.
         """
-        Closes the connection. Releases all qubits and remove the app ID from the used app IDs.
-        """
+
+        # Flush all remaining commands
+        if self._pending_headers:
+            self.flush()
+
         if release_qubits:
             self.release_all_qubits()
         self._s.close()
