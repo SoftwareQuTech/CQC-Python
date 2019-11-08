2b3a0099ca8c8951415122f205eff67781b266e7
Liam <11602015+Shyadow@users.noreply.github.com>
2019-11-08 10:27:33 +0100

    Test remaining pending headers get flushed when exiting cqc context.
    When pending_messages = True, any remaining pending headers should get
    flushed when you exit the cqc context. A test has been added to test
    this.

--- tests/test_cqcconnection.py
+++ tests/test_cqcconnection.py
@@ -2,7 +2,8 @@ import pytest
 
 from cqc.pythonLib import CQCConnection, qubit
 from cqc.cqcHeader import CQCHeader, CQCCmdHeader, CQC_TP_COMMAND,\
-    CQC_CMD_HDR_LENGTH, CQC_CMD_H, CQC_CMD_NEW, CQC_CMD_RELEASE
+    CQC_CMD_HDR_LENGTH, CQC_CMD_H, CQC_CMD_NEW, CQC_CMD_RELEASE, \
+    CQC_CMD_X
 
 from utilities import get_header
 
@@ -58,8 +59,77 @@ def commands_to_apply_simple_h(cqc):
     q.H()
 
 
+def commands_to_apply_flush(cqc):
+    """Test if CQCConnection automatically flushes when exiting context."""
+    cqc.set_pending(True)
+    q = qubit(cqc)
+    q.H()
+    q.X()
+
+
+def get_expected_headers_flush():
+    """What headers we expect."""
+    hdr_tp_cmd = get_header(
+        CQCHeader, 
+        version=2,
+        tp=CQC_TP_COMMAND,
+        app_id=0,
+        length=CQC_CMD_HDR_LENGTH,
+    )
+    hdr_cmd_new = get_header(
+        CQCCmdHeader, 
+        qubit_id=0,
+        instr=CQC_CMD_NEW,
+        notify=True,
+        action=False,
+        block=True,
+    )
+    hdr_tp_cmd_2 = get_header(
+        CQCHeader, 
+        version=2,
+        tp=CQC_TP_COMMAND,
+        app_id=0,
+        length=2*CQC_CMD_HDR_LENGTH,
+    )
+    hdr_cmd_h = get_header(
+        CQCCmdHeader, 
+        qubit_id=0,
+        instr=CQC_CMD_H,
+        notify=True,
+        action=False,
+        block=True,
+    )
+    hdr_cmd_x = get_header(
+        CQCCmdHeader,
+        qubit_id=0,
+        instr=CQC_CMD_X,
+        notify=True,
+        action=False,
+        block=True,
+    )
+    hdr_cmd_release = get_header(
+        CQCCmdHeader, 
+        qubit_id=0,
+        instr=CQC_CMD_RELEASE,
+        notify=True,
+        action=False,
+        block=True,
+    )
+
+    expected_headers = [
+        hdr_tp_cmd,
+        hdr_cmd_new,
+        hdr_tp_cmd_2,
+        hdr_cmd_h,
+        hdr_cmd_x,
+        hdr_tp_cmd + hdr_cmd_release
+    ]
+
+    return expected_headers
+
 @pytest.mark.parametrize("commands_to_apply, get_expected_headers", [
     (commands_to_apply_simple_h, get_expected_headers_simple_h),
+    (commands_to_apply_flush, get_expected_headers_flush)
 ])
 def test_commands(commands_to_apply, get_expected_headers, monkeypatch, mock_socket, mock_read_message):
 
